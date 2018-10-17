ac20dc1ccc625fccc20a5a2405a69d0eed5fc385
Konstantin <grahovsky@gmail.com>
2018-10-17 17:03:49 +0300

    #touch.location #moveAction

--- SpaceAdventure/GameScene.swift
+++ SpaceAdventure/GameScene.swift
@@ -11,11 +11,33 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    //создаем свойство
+    var spaceShip: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
-        let spaceShip = SKSpriteNode(imageNamed: "spaceShip")
         
+        // инициализируем свойство
+        spaceShip = SKSpriteNode(imageNamed: "spaceShip")
+        
+        // определяем позицию spaceShip на экране
         spaceShip.position = CGPoint(x: 100, y: 100)
         
+        // добавляем
         addChild(spaceShip)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+          
+            // определяем точку прикосновения с экраном
+            let touchLocation = touch.location(in: self)
+            
+            // создаем действие и применяем его
+            let moveAction = SKAction.move(to: touchLocation, duration: 1.0)
+            
+            spaceShip.run(moveAction)
+        }
+        
     }
 }
